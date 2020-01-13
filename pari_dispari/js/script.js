81619588988def60c3af2fc36ef12549980b963d
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-13 16:49:59 +0100

    Commenti su pari e dispari

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -1,31 +1,31 @@
-// Gioco Pari e dispari:
-// L’utente sceglie pari o dispari e un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1
-// a 5) per il computer.
-// Sommiamo i due numeri e dichiariamo chi ha
-// vinto (controlliamo se la somma da pari o dispari).
+// Gioco Pari e dispari
 
 
+// L’utente sceglie pari o dispari
 var sceltaUtente = prompt('Pari o Dispari?');
-
 while (sceltaUtente.length == 0 || sceltaUtente != 'Dispari' && sceltaUtente != 'Pari') {
     var sceltaUtente = prompt('Devi scrivere Pari o Dispari.');
 }
 
+// L’utente sceglie e un numero da 1 a 5.
 var numeroUtente = parseInt(prompt('Tira un numero da 1 a 5.'));
 while (isNaN(numeroUtente)) {
     var numeroUtente = parseInt(prompt('Devi inserire un numero da 1 a 5.'));
 }
-
+// Generiamo un numero random per il computer.
 var numeroRandom = Math.floor(Math.random() * 5 + 1);
+
+// Sommiamo i due numeri
 var somma = numeroUtente + numeroRandom;
 
+// controlliamo se la somma da pari o dispari
 if (somma % 2 == 0) {
   var pari = true;
 } else {
   var pari = false;
 }
 
+// e dichiariamo chi ha vinto
 if (sceltaUtente == 'Pari' && pari == true) {
   var messaggio = 'Complimenti, hai vinto!'
 } else if (sceltaUtente == 'Dispari' && pari == false) {
